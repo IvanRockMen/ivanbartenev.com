6dbbfcedd94b3ccc0dac2a9b842d0d071a606fce
ivanrockmen <bartenev.ivan.a@gmail.com>
2021-07-07 22:13:50 +0800

    Wirhout secret key

--- ivanbartenev/settings.py
+++ ivanbartenev/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ln&tq=gp1wvur8&)7frk+vm-x^*ag@6^q!c3o$kv&y^+wobb(6'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
